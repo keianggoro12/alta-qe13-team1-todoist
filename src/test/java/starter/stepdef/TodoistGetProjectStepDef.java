0906c786ee445df188473a248f97dbc2f571b360
keianggoro12 <keianggoro12@gmail.com>
2023-12-29 22:01:42 +0700

    update get and create project automation

--- src/test/java/starter/stepdef/TodoistGetProjectStepDef.java
+++ src/test/java/starter/stepdef/TodoistGetProjectStepDef.java
@@ -5,12 +5,15 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
 import starter.todoist.TodoistGetProjectAPI;
 import starter.todoist.TodoistResponses;
 import starter.utils.Constants;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import java.io.File;
+
+import static net.serenitybdd.rest.SerenityRest.then;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TodoistGetProjectStepDef {
@@ -22,16 +25,21 @@ public class TodoistGetProjectStepDef {
     public void getAllProject() {todoistGetProjectAPI.setGetAllProject();
     }
 
+
     @When("Send request get all project")
     public void sendRequestGetProject() {
         SerenityRest.given()
-                .header("Authorization", "Bearer " + Constants.TOKEN_KEI)
+          .header("Authorization", "Bearer " + Constants.TOKEN_KEI)
                 .when().get(TodoistGetProjectAPI.GET_ALL_PROJECT);
+    }
+    @When("Send request get single project")
+    public void sendRequestSingleProject(){
+    SerenityRest.when().get(TodoistGetProjectAPI.GET_SINGLE_PROJECT);
     }
 
     @Then("Status code should be {int}")
     public void statusCodeShouldBe(int statusCode) {
-        SerenityRest.then()
+        then()
                 .statusCode(statusCode);
     }
 
@@ -39,5 +47,15 @@ public class TodoistGetProjectStepDef {
     public void validateGetProjectJsonSchema(String json) {
         File jsonProject = new File(Constants.JSON_SCHEMA+json);
         SerenityRest.and().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonProject));
+    }
+
+    @Given("Get single project with {string}")
+    public void getSingleProjectValid(String project_id) {
+        todoistGetProjectAPI.setGetSingleProject(project_id);
+    }
+
+    @And("Response body should {string}")
+    public void responseBodyShould(String response) {
+        then().body(equalTo(response));
     }
 }
--- src/test/resources/features.todoist/GetProject.feature
+++ src/test/resources/features.todoist/GetProject.feature
@@ -5,4 +5,44 @@ Feature: Get project
     Given Get all project
     When Send request get all project
     Then Status code should be 200
-    And Validate get project json schema "GetProjectSchema.json"
+    And Validate get project json schema "GetAllProjectSchema.json"
+
+   #GSP002
+  Scenario Outline: Get single project with valid project_id
+    Given Get single project with "<project_id>"
+    When Send request get single project
+    Then Status code should be 200
+    And Validate get project json schema "GetSingleProjectSchema.json"
+    Examples:
+      | project_id |  |
+      | 2325681395 |  |
+
+    #GSP003
+  Scenario Outline: Get single project with invalid project_id
+    Given Get single project with "<project_id>"
+    When Send request get single project
+    Then Status code should be 404
+    And Response body should "Project not found"
+    Examples:
+      | project_id |  |
+      | 23172312313123123121 |  |
+
+    #GSP004
+  Scenario Outline: Get single project with unregistered project_id
+    Given Get single project with "<project_id>"
+    When Send request get single project
+    Then Status code should be 404
+    And Response body should "Project not found"
+    Examples:
+      | project_id |  |
+      | 2325681396 |  |
+
+    #GSP005
+  Scenario Outline: Get single project without project_id
+    Given Get single project with "<project_id>"
+    When Send request get single project
+    Then Status code should be 404
+    And Response body should "Project not found"
+    Examples:
+      | project_id |  |
+      |            |  |
